83451cae715a6dd67857fe0063555a5551feeb12
AsthanaShivanshi <shivanshi.asthana@unil.ch>
2025-03-30 14:02:47 +0200

    remapping file added

---
+++ remapping.py
@@ -0,0 +1,38 @@
+
+#Regridding files to 12 kms resolution (not conservatively remapped for now)
+import os
+import pandas as pd
+import numpy as np
+import xarray as xr 
+import subprocess
+
+#Loading the data and specifying the directories
+input_dir = '/Users/sasthana/Documents/Downscaling/data/Targets_RhiresD_TabsD'
+output_dir = '/Users/sasthana/Documents/Downscaling/data/Inputs_regridded_RhiresD_TabsD'
+
+#Defining the grid resolution file in .txt format
+resolution_spec_file= '/Users/sasthana/Documents/data/grid_12_kms_file.txt'
+
+#Regridding loop after confirming files exist and are accessible
+if filename.endswith(".nc"):
+    in_file = os.path.join(input_dir, filename)
+    out_file= os.path.join(output_dir, f"remapped_{filename}")
+
+#Using cdo for remapping using bilinear interpolation
+    cdo_command = [
+            'cdo', 
+            'remapbil,' + grid_file, 
+            input_file, 
+            output_file
+        ]
+    try:
+        subprocess.run(cdo_command, check=True)
+        print(f'regridding successfor {filename}')
+        #Exception Handling
+    except subprocess.CalledProcessError as e:
+        print(f'Error regridding {filename}: {e}')
+
+
+
+
+
