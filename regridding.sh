049a5a25cc8a685d2199e0bd190149ce33f4dd24
sasthana <shivanshi.asthana@unil.ch>
2025-03-30 20:24:55 +0200

    Changes

--- regridding.sh
+++ regridding.sh
@@ -12,13 +12,13 @@ mkdir -p "$OUTPUT_DIR"
 # Loop through all .nc files in the input directory
 for file in "$INPUT_DIR"/*.nc; do
     filename=$(basename "$file")
-    output_file="$OUTPUT_DIR/remapped_con_11km_${filename}"
+    output_file="$OUTPUT_DIR/remapped_11km_${filename}"
     temp_regrid="${OUTPUT_DIR}/temp_regrid_${filename}"
 
     echo "Processing $filename ..."
 
     # Step 1: Regridding using bilinear interpolation to the grid of tas_11km_limited.nc
-    cdo remapcon,"$REFERENCE_GRID" "$file" "$temp_regrid"
+    cdo remapbil,"$REFERENCE_GRID" "$file" "$temp_regrid"
 
     # Step 2: Save the regridded output
     mv "$temp_regrid" "$output_file"
