7f40caa232e0cf26b8702083a0db1dd54a8d564d
sasthana <shivanshi.asthana@unil.ch>
2025-03-30 18:59:04 +0200

    Changes

--- regridding.sh
+++ regridding.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
-#SBATCH --job-name=regrid_cdo         # Job name
-#SBATCH --output=regrid_cdo_%j.out    # Output file
-#SBATCH --error=regrid_cdo_%j.err     # Error file
-#SBATCH --ntasks=1                    # Number of tasks
-#SBATCH --time=01:00:00               # Time limit
-#SBATCH --mem=4GB                     # Memory allocation
 
-# Load the necessary modules
-module load python3 # Adjust based on the Python version you are using
-module load cdo
+# Directories
+INPUT_DIR="/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/data/Targets_Rhires_TabsD"
+OUTPUT_DIR="/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/data/Inputs_regridded_RhiresD_TabsD"
+GRID_FILE="/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/data/tas_11km_Switzerland.nc"
 
-# Run the Python script
-python3 regridding.py
+# Lon and Lat extents of the final output files
+LON_MIN=5.75553
+LON_MAX=10.70298
+LAT_MIN=45.64363
+LAT_MAX=48.06181
+
+
+# Looping through all .nc files in the Targets_RhiresD_tabsD directory
+
+for file in "$INPUT_DIR"/*.nc; do
+    # Extracting the filename
+    filename=$(basename "$file")
+
+    # Defining the output filename
+    output_file="$OUTPUT_DIR/remapped_11km_${filename}"
+
+    echo "Processing $filename ..."
+
+    # Step 1: Regridding using bilinear interp.
+    temp_regrid="${OUTPUT_DIR}/temp_regrid_${filename}"
+    cdo remapbil,"$GRID_FILE" "$file" "$temp_regrid"
+
+    # Step 2: Cropping to the required lat/lon extent
+    cdo sellonlatbox,$LON_MIN,$LON_MAX,$LAT_MIN,$LAT_MAX "$temp_regrid" "$output_file"
+
+    # Removing temporary file
+    rm "$temp_regrid"
+
+    echo "Finished processing: $output_file"
+done
+
+echo "All files processed successfully!"
