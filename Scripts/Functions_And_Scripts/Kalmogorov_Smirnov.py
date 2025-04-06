af36f8ac660729adf7b9a6562ff0bc5544318300
sasthana <shivanshi.asthana@unil.ch>
2025-04-06 20:15:41 +0200

    Changes

--- Scripts/Functions_And_Scripts/Kalmogorov_Smirnov.py
+++ Scripts/Functions_And_Scripts/Kalmogorov_Smirnov.py
@@ -16,39 +16,38 @@ def kalmogorov_smirnov(file1, var1, file2, var2, title=None):
     ds2= xr.open_dataset(file2)
 
     #Extracting the gridded variables
-
     v1= ds1[var1].values
     v2= ds2[var2].values
 
-    #Numpy arrays to store gridded values of KS stat and p values 
+    # Numpy arrays to store gridded values of KS stat and p values 
+    ks_statistic_gridded = np.full((v1.shape[1], v1.shape[2]), np.nan)
+    p_value_gridded = np.full((v1.shape[1], v1.shape[2]), np.nan)
 
-    ks_statistic_gridded = np.full ((lat,lon),np.nan)
+    # Regridding operation (might be required)
+    if v1.shape != v2.shape:
+        if 'lat' in v2.dims and 'lon' in v2.dims:
+            v2 = v2.interp(lat=v1.lat, lon=v1.lon, method="nearest")
+        elif 'y' in v2.dims and 'x' in v2.dims:
+            v2 = v2.interp(y=v1.y, x=v1.x, method="nearest")
+        else:
+            raise ValueError("Unable to regrid: coordinate names not recognized")
 
-    p_value_gridded = np.full((lat,lon), np.nan)
+    # Looping over grid cells
+    for i in range(v1.shape[1]):
+        for j in range(v1.shape[2]):
+            v1_ts = v1[:, i, j]
+            v2_ts = v2[:, i, j]
 
-    #Regridding operation (might be required)
+            # Removing NaNs in case there are any
+            mask = ~np.isnan(v1_ts) & ~np.isnan(v2_ts) 
+            v1_ts = v1_ts[mask]
+            v2_ts = v2_ts[mask]
 
-    if v1.shape!= v2.shape:
-        v2= v2.interp_like (v1, method = "nearest")
+            ks_statistic, p_value = stats.ks_2samp(v1_ts, v2_ts)
+            ks_statistic_gridded[i, j] = ks_statistic
+            p_value_gridded[i, j] = p_value
 
-    #Looping over grid cells 
-
-    for i in range (lat):
-        for j in range(lon):
-            v1_ts= v1[:,i,j]
-            v2_ts= v2[:,i,j]
-
-            #In case of any NaNs , remove them
-            mask= ~np.isnan(v1_ts) & ~np.isnan(v2_ts) 
-            v1_ts= v1_ts [mask]
-            v2_ts= v2_ts [mask]
-
-            ks_statistic, p_value = stats.ks_2samp(v1_ts,v2_ts)
-            ks_statistic_gridded[i,j]= ks_statistic
-            p_value_gridded[i,j]= p_value
-
-
-    #Plots for KS statistic and p values for two distributions
+    # Plots for KS statistic and p values
     plt.figure(figsize=(10, 6))
     plt.imshow(ks_statistic_gridded, cmap='viridis')
     plt.colorbar(label='Gridded KS Statistic for distributions {v1} and {v2}')
@@ -58,7 +57,7 @@ def kalmogorov_smirnov(file1, var1, file2, var2, title=None):
     plt.tight_layout()
     plt.show()
 
-    # Plotting the p-value grid
+    # Plotting the gridded p-values
     plt.figure(figsize=(10, 6))
     plt.imshow(p_value_gridded, cmap='coolwarm', origin='lower', aspect='auto')
     plt.colorbar(label='p-value gridded')
@@ -68,7 +67,5 @@ def kalmogorov_smirnov(file1, var1, file2, var2, title=None):
     plt.tight_layout()
     plt.show()
 
-    # Return the KS statistic and p-value grids for further analysis
+    # returning the gridded KS statistic and p-values
     return ks_statistic_gridded, p_value_gridded
-
-
