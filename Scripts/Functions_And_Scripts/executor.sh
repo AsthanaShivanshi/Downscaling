e2d769678c6a70664284e15cb0f06b1816a97014
sasthana <shivanshi.asthana@unil.ch>
2025-04-07 10:55:30 +0200

    Changes

--- Scripts/Functions_And_Scripts/executor.sh
+++ Scripts/Functions_And_Scripts/executor.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#SBATCH --job-name=my_job       # Name of the job
+#SBATCH --output=job_output.txt # Standard output file
+#SBATCH --error=job_error.txt   # Standard error file
+#SBATCH --ntasks=1              # Number of tasks (typically 1 for single-node jobs)
+#SBATCH --cpus-per-task=4      # Number of CPUs per task (adjust as needed)
+#SBATCH --time=02:00:00         # Time limit (format: HH:MM:SS)
+#SBATCH --mem=8G                # Memory required (adjust as needed)
 
 # Define the input files and variables
 file1="/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/data_and_outputs/TabsD_1961_2023.nc"
