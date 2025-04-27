1d1dd80b05449e0a50007aa6487520c4e443e732
sasthana <shivanshi.asthana@unil.ch>
2025-04-27 22:32:09 +0200

    Changes

---
+++ Scripts/Functions/R_squared.py
@@ -0,0 +1,28 @@
+import xarray as xr
+import numpy as np
+
+def gridded_R_squared(pred_path, truth_path, var1, var2, chunk_size={'time': 50}):
+    """
+    Calculate grid-wise coefficient of determination (R^2) between two gridded variables, ignoring NaNs.
+    """
+    # Load datasets with chunks
+    ds_pred = xr.open_dataset(pred_path, chunks=chunk_size)
+    ds_true = xr.open_dataset(truth_path, chunks=chunk_size)
+    
+    # Align the variables
+    var1_data, var2_data = xr.align(ds_pred[var1], ds_true[var2])
+    
+    # Mask valid data (non-NaN)
+    valid_mask = (~np.isnan(var1_data)) & (~np.isnan(var2_data))
+    
+    # Residual sum of squares
+    ss_res = ((var1_data - var2_data) ** 2).where(valid_mask).sum(dim='time')
+    
+    # Total sum of squares
+    mean_true = var2_data.where(valid_mask).mean(dim='time')
+    ss_tot = ((var2_data - mean_true) ** 2).where(valid_mask).sum(dim='time')
+    
+    r2 = 1 - (ss_res / ss_tot)
+    r2 = r2.astype(np.float32)
+    
+    return r2
