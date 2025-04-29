0755cd24129facc9ffff53c3f2a91948253d4af0
sasthana <shivanshi.asthana@unil.ch>
2025-04-29 15:44:12 +0200

    Changes

--- ML_models/config.py
+++ ML_models/config.py
@@ -2,10 +2,10 @@ CONFIG = {
     "batch_size": 32,
     "num_workers": 4,
     "input_paths": {
-        "precip_input": "path/to/RhiresD_1km_bicubic_Swiss_features_masked.nc",
-        "temp_input": "path/to/TabsD_1km_bicubic_Swiss_features_masked.nc",
-        "precip_target": "path/to/RhiresD_1971_2022.nc",
-        "temp_target": "path/to/TabsD_1971_2022.nc",
+        "precip_input": "/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/data/processed/RhiresD_1km_bicubic_Swiss_features_masked.nc",
+        "temp_input": "/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/data/processed/TabsD_1km_bicubic_Swiss_features_masked.nc",
+        "precip_target": "/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/data/raw/RhiresD_1971_2022.nc",
+        "temp_target": "/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/data/raw/TabsD_1971_2022.nc",
     },
     "var_names": {
         "precip_input": "pr",
