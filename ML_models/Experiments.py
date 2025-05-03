6c7ff1c49736d65aa2b7cc5dce1296b64d25fa46
sasthana <shivanshi.asthana@unil.ch>
2025-05-03 13:10:47 +0200

    All changes synced

--- ML_models/Experiments.py
+++ ML_models/Experiments.py
@@ -7,7 +7,7 @@ from Train import train_model
 from Train import checkpoint_save
 import os
 
-def run_experiment(train_dataset, val_dataset, quick_test=False, num_epochs=30):
+def run_experiment(train_dataset, val_dataset, quick_test=True, num_epochs=30):
     model = UNet(in_channels=2, out_channels=2)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     criterion = nn.MSELoss()
@@ -15,13 +15,13 @@ def run_experiment(train_dataset, val_dataset, quick_test=False, num_epochs=30):
     if quick_test:
         train_loader = torch.utils.data.DataLoader(
             torch.utils.data.Subset(train_dataset, range(100)),
-            batch_size=16, shuffle=True
+            batch_size=32, shuffle=True
         )
         val_loader = torch.utils.data.DataLoader(
-            torch.utils.data.Subset(val_dataset, range(50)),
-            batch_size=16, shuffle=False
+            torch.utils.data.Subset(val_dataset, range(30)), #Using a third of the samples for validation because I need to get a realistic picture according to the train test val split samples I actually have 
+            batch_size=32, shuffle=False
         )
-        num_epochs = 10
+        num_epochs = 20
     else:
         train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
         val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=False)
