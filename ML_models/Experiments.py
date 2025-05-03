50d6f3e85a1f9369fa5b23480e5199a27b6dcca5
sasthana <shivanshi.asthana@unil.ch>
2025-05-03 23:00:47 +0200

    Changes

--- ML_models/Experiments.py
+++ ML_models/Experiments.py
@@ -15,16 +15,16 @@ def run_experiment(train_dataset, val_dataset, quick_test=False, num_epochs=30):
     if quick_test:
         train_loader = torch.utils.data.DataLoader(
             torch.utils.data.Subset(train_dataset, range(100)),
-            batch_size=32, shuffle=True
+            batch_size=64, shuffle=True
         )
         val_loader = torch.utils.data.DataLoader(
             torch.utils.data.Subset(val_dataset, range(30)), #Using a third of the samples for validation because I need to get a realistic picture according to the train test val split samples I actually have 
-            batch_size=32, shuffle=False
+            batch_size=64, shuffle=False
         )
         num_epochs = 20
     else:
-        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
-        val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=False)
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+        val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=False)
 
     trained_model, history = train_model(
         model,
@@ -38,5 +38,5 @@ def run_experiment(train_dataset, val_dataset, quick_test=False, num_epochs=30):
 
     #### Saving the model is optional uncomment if requiredd
     final_val_loss= history['val_loss'][-1]
-    checkpoint_save(model, optimizer, epoch=num_epochs, loss= final_val_loss, path="/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/checkpoints/All_Samples_64BS_model_checkpoint_UNet_01.pth")
+    checkpoint_save(model, optimizer, epoch=num_epochs, loss= final_val_loss, path="/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/checkpoints/All_Samples_64BS_30epochs_model_checkpoint_UNet_01.pth")
     return trained_model, history, final_val_loss
