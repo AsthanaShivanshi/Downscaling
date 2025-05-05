ff02115feb85dfd1f0a83d4a08e93bef57827860
sasthana <shivanshi.asthana@unil.ch>
2025-05-05 17:48:19 +0200

    Changes

--- Scripts/Functions/Standardise.py
+++ Scripts/Functions/Standardise.py
@@ -5,16 +5,16 @@ import numpy as np
 def norm_params(ds, var_name):
     """Calculating the gridwise mean and standard deviation for a given variable assuming a normal distribtuion (to be used for Tmeperature"""
     var= ds[var_name]
-    mean= var.mean(dim="time", skipna=True)
-    std= var.std(dim="time", skipna=True)
+    mean= var.mean(dim="time", skipna=True).compute()
+    std= var.std(dim="time", skipna=True).compute()
 
     return mean, std
 
 def min_max_calculator(ds, var_name):
     "Calcualting the gridwise min and max for a given variable(to be used for non normally distributed variales such as precipitation)"""
     var= ds[var_name]
-    min= var.min(dim="time", skipna=True)
-    max= var.max(dim="time", skipna=True)
+    min= var.min(dim="time", skipna=True).compute()
+    max= var.max(dim="time", skipna=True).compute()
 
     return min,max
 
