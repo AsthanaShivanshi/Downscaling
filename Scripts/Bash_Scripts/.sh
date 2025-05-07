a28a8b00a2ddac8249d4ca1abd82be3bca3c20f3
sasthana <shivanshi.asthana@unil.ch>
2025-05-07 21:58:41 +0200

    Changes

---
+++ Scripts/Bash_Scripts/.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=UNet_training       
+#SBATCH --output=job_output-%j.txt 
+#SBATCH --error=job_error-%j.txt  
+#SBATCH --ntasks=1             
+#SBATCH --cpus-per-task=4     
+#SBATCH --time=3-00:00:00         
+#SBATCH --mem=32G  
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+              
+
+module load python
+
+source /work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/MyPythonEnvironment/bin/activate
+
+#Directory containing the pipeline
+cd /work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/ML_models
+
+
+#For quick test module uncomment
+#python Main.py --quick_test
+
+#For full training modzle utilisattion uncomment
+#Model checkpoint will be saved at 20 epochs, then will continue from there to avoid losing everything
+
+python Main.py
