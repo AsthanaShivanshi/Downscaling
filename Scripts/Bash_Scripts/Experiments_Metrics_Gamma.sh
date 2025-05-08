587536856bd833ad286c826c581c6f935b3c1136
sasthana <shivanshi.asthana@unil.ch>
2025-05-08 13:57:10 +0200

    Changes to Gamma bash script

--- Scripts/Bash_Scripts/Experiments_Metrics_Gamma.sh
+++ Scripts/Bash_Scripts/Experiments_Metrics_Gamma.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=Gamma_tests     
+#SBATCH --job-name=Gaussian_tests     
 #SBATCH --output=job_output-%j.txt 
 #SBATCH --error=job_error-%j.txt  
-#SBATCH --ntasks=1             
-#SBATCH --cpus-per-task=4     
-#SBATCH --time=3-00:00:00         
-#SBATCH --mem=64G  
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1              
+#SBATCH --cpus-per-task=4         # 4 CPU cores (Dask + blocks)
+#SBATCH --time=3-00:00:00        
+#SBATCH --mem=64G                
+#SBATCH --partition=cpu         
+# (NO --gres=gpu:1 )
               
 
 module load python
