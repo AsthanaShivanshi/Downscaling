a55c6a2dca0c0213a8df0d3c9f0605b2419748a7
sasthana <shivanshi.asthana@unil.ch>
2025-05-11 20:32:30 +0200

    Changes

--- ML_models/UNet_Deterministic/UNet_Experiments.sh
+++ ML_models/UNet_Deterministic/UNet_Experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=UNet_training_100_samples_Triangular2_LR    
-#SBATCH --output=/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/ML_models/UNet_Deterministic/job_output-Cyclical_LR-%j.txt 
-#SBATCH --error=/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/ML_models/UNet_Deterministic/job_error-Cyclical_LR%j.txt  
+#SBATCH --job-name=UNet_training_100_samples_ReduceLROnPlateau    
+#SBATCH --output=/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/ML_models/UNet_Deterministic/job_output-Reduce_LROnPlateau-%j.txt 
+#SBATCH --error=/work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling/ML_models/UNet_Deterministic/job_error-Reduce_LROnPlateau%j.txt  
 #SBATCH --ntasks=1             
 #SBATCH --cpus-per-task=4     
 #SBATCH --time=3-00:00:00         
