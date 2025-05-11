08001602982e371ba47bc9f26f11841d08c246e5
sasthana <shivanshi.asthana@unil.ch>
2025-05-11 20:45:19 +0200

    Changes

--- ML_models/UNet_Deterministic/Train.py
+++ ML_models/UNet_Deterministic/Train.py
@@ -1,3 +1,11 @@
+
+#In present version of Train.py depemding on the type of learning scheduler used
+
+#If CyclicalLR: per batch stepping, 2 times per cycle 
+#If ReduceLROnPlateau : per epoch stepping using validation loss
+#Gradient norm logging included to wexplore vanishing/exploding gradients
+#Learning rate also logged per every 20 batches for each epoch. 
+
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -5,6 +13,8 @@ from tqdm import tqdm
 import wandb
 
 def train_one_epoch(model, dataloader, optimizer, criterion, scheduler=None, config=None):
+    import torch.optim.lr_scheduler as lrs
+
     model.train()
     running_loss = 0.0
     quick_test = config["experiment"].get("quick_test", False)
@@ -23,19 +33,22 @@ def train_one_epoch(model, dataloader, optimizer, criterion, scheduler=None, con
         grad_norm = total_norm ** 0.5
 
         optimizer.step()
-        if scheduler:
+
+        if scheduler and not isinstance(scheduler, lrs.ReduceLROnPlateau):
             scheduler.step()
 
         running_loss += loss.item()
 
-        # Log learning rate after every 20 batches as well in addition to the epoch loss
+        # Logging per 20 batches for every epocvh
         if i % 20 == 0:
             log_dict = {
                 "train_loss_batch": loss.item(),
                 "grad_norm": grad_norm
             }
-            if scheduler:
+            if scheduler and not isinstance(scheduler, lrs.ReduceLROnPlateau):
                 log_dict["lr"] = scheduler.get_last_lr()[0]
+            else:
+                log_dict["lr"] = optimizer.param_groups[0]["lr"]
             wandb.log(log_dict)
 
         if quick_test and i == 2:
@@ -60,7 +73,10 @@ def validate(model, dataloader, criterion, config=None):
 
     return running_loss / (j + 1)
 
+
 def train_model(model, train_loader, val_loader, optimizer, criterion, scheduler=None, config=None):
+    import torch.optim.lr_scheduler as lrs
+
     train_cfg = config["train"]
     num_epochs = train_cfg.get("num_epochs", 30)
     checkpoint_path = train_cfg.get("checkpoint_path", "best_model.pth")
@@ -79,27 +95,27 @@ def train_model(model, train_loader, val_loader, optimizer, criterion, scheduler
 
         print(f"Train Loss: {train_loss:.3f} | Val Loss: {val_loss:.3f}")
 
-        #Custom scheduler depending on the yaml file
+        # Step scheduler
         if scheduler:
-            import torch.optim.lr_scheduler as lrs
-            if isinstance(scheduler,lrs.ReduceLROnPlateau):
+            if isinstance(scheduler, lrs.ReduceLROnPlateau):
                 scheduler.step(val_loss)
-                current_lr=optimizer.param_group[0]["lr"]
             else:
                 scheduler.step()
-                current_lr=scheduler.get_last_lr()[0]
+            current_lr = optimizer.param_groups[0]["lr"]
         else:
-            current_lr= optimizer.param_group[0]["lr"]
+            current_lr = optimizer.param_groups[0]["lr"]
 
+        # Save best model
         if val_loss < best_val_loss:
             best_val_loss = val_loss
             checkpoint_save(model, optimizer, epoch+1, val_loss, checkpoint_path)
 
-        wandb.log({"epoch": epoch+1,
-                "train_loss_epoch":train_loss,
-                "val_loss_epoch": val_loss,
-                "lr_epoch": current_lr
-                })
+        wandb.log({
+            "epoch": epoch+1,
+            "train_loss_epoch": train_loss,
+            "val_loss_epoch": val_loss,
+            "lr_epoch": current_lr
+        })
 
     return model, history
 
